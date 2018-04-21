0e56277b5e8d96daa384fdb8f016939880c3abed
mparszewski <33162624+mparszewski@users.noreply.github.com>
2018-04-21 12:18:03 +0200

    initial

---
+++ src/main/java/com/example/demo/BestHackathonApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestHackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestHackathonApplication.class, args);
+    }
+}
