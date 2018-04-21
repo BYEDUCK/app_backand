2e207916541a1e4e30bf023e70be12974a180b3e
Mateusz Marian Bajdak <m.bajdak@stud.elka.pw.edu.pl>
2018-04-21 23:20:53 +0200

    Enabling deleting user from lecture

--- src/main/java/com/example/demo/controller/UserLectures/UserLecturesController.java
+++ src/main/java/com/example/demo/controller/UserLectures/UserLecturesController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller.UserLectures;
 
+import com.example.demo.exceptions.ForbiddenAccessException;
 import com.example.demo.service.UserLectureService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +22,11 @@ public class UserLecturesController {
     @PostMapping(value = "/user_lectures")
     public void assignUserToLecture(@RequestBody UserLecturesRequest request){
         service.assignUserToLecture(request);
+    }
+
+    @DeleteMapping(value = "user_lectures")
+    public void removeUserFromLecture(@RequestBody UserLecturesRequest request) throws ForbiddenAccessException {
+        service.deleteUserFromLecture(request);
     }
 
 }
