60e5b773cf84d9ff97e308ed53e64cca21bf4fa6
Kacper Roszczyna <kacper.ros94@gmail.com>
2018-04-21 21:31:48 +0200

    fixed date

--- src/main/java/com/example/demo/controller/Questions/QuestionController.java
+++ src/main/java/com/example/demo/controller/Questions/QuestionController.java
@@ -4,6 +4,7 @@ package com.example.demo.controller.Questions;
 import com.example.demo.exceptions.ObjectNotFoundException;
 import com.example.demo.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -31,21 +32,22 @@ public class QuestionController {
 
     @GetMapping(value = "/questions")
     public List<QuestionResponse> getQuestionsForLectureAfter(@RequestParam("lectureId") int lectureId,
-                                                              @RequestParam("after") LocalDateTime after,
+                                                              @RequestParam(value = "after", required = false)
+                                                              @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime after,
                                                               @RequestParam("published") boolean published) throws ObjectNotFoundException {
-        if (after == null){
-            after = LocalDateTime.of(2006,5,12,5,15);
+        if (after == null) {
+            after = LocalDateTime.of(2006, 5, 12, 5, 15);
         }
         return service.findByLectureIdAfterTime(lectureId, after, published);
     }
 
     @PutMapping(value = "/questions")
     public QuestionResponse edit(@RequestBody UpdateQuestionRequest request) throws ObjectNotFoundException {
-       return service.update(request);
+        return service.update(request);
     }
 
     @DeleteMapping(value = "/questions/{questionId}")
-    public void delete(@PathVariable("questionId") int questionId){
+    public void delete(@PathVariable("questionId") int questionId) {
         service.delete(questionId);
     }
 
